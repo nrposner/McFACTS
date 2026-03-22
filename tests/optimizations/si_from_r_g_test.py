eab57181ace3a9dbd667620052fb6dba77f74636
nrposner <nicolasposner@gmail.com>
2026-03-22 15:09:45 +0100

    expanding test cases

--- tests/optimizations/si_from_r_g_test.py
+++ tests/optimizations/si_from_r_g_test.py
@@ -4,11 +4,11 @@ import astropy.units as u
 from mcfacts.physics.point_masses import si_from_r_g, si_from_r_g_optimized
 
 
-# units = [1, u.g, u.kg, u.solMass, u.jupiterMass, u.earthMass]
-units = [1]
+# at present, the original si_from_r_g function cannot take in length or mass units as variants for the distance_r_g argument, it needs to be a scalar
+units = [1, u.g, u.kg, u.solMass, u.jupiterMass, u.earthMass]
 
 # and the array case
-vals = [1.0, 5435.46345, 48.5, 137.0, 0.00001, np.array([0.0, 1.0, 5435.46345, 48.5, 137.0, 0.00001])]
+vals = [1.0, 5435.46345, 48.5, 137.0, 0.00001, np.array([1.0, 5435.46345, 48.5, 137.0, 0.00001])]
 
 smbh_mass = 1.0
 
@@ -17,8 +17,8 @@ def test_si_from_rg():
         # notable difference: r_g_from units returns unitless values, while optimized 
         # returns values in meters. Since the vast majority of uses of this function return
 
-        orig = si_from_r_g(smbh_mass, val * unit)
-        opt = si_from_r_g_optimized(smbh_mass, val * unit)
+        orig = si_from_r_g(smbh_mass * unit, val)
+        opt = si_from_r_g_optimized(smbh_mass * unit, val)
 
         assert(np.allclose(orig, opt, rtol=1e-6))
 
