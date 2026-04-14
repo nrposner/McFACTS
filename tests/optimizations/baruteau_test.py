4aff3d6b41d75843e8919fcf65b123b771933686
nrposner <nicolasposner@gmail.com>
2026-04-14 12:26:18 -0400

    fixing si_from_r_g tests
    removing commented code

--- tests/optimizations/baruteau_test.py
+++ tests/optimizations/baruteau_test.py
@@ -96,25 +96,3 @@ def test_sweep_optimized_matches_original(
     assert np.allclose(out_time_to_merger_gw, out_time_to_merger_gw_opt, rtol=1e-6)
 
 
-
-
-# # units = [1, u.g, u.kg, u.solMass, u.jupiterMass, u.earthMass]
-# units = [1]
-#
-# # and the array case
-# vals = [1.0, 5435.46345, 48.5, 137.0, 0.00001, np.array([0.0, 1.0, 5435.46345, 48.5, 137.0, 0.00001])]
-#
-# smbh_mass = 1.0
-#
-# def test_si_from_rg():
-#     for val, unit in zip(vals, units):
-#         # notable difference: r_g_from units returns unitless values, while optimized 
-#         # returns values in meters. Since the vast majority of uses of this function return
-#
-#         orig = si_from_r_g(smbh_mass, val * unit)
-#         opt = si_from_r_g_optimized(smbh_mass, val * unit)
-#
-#         assert(np.allclose(orig, opt, rtol=1e-6))
-#
-
-
